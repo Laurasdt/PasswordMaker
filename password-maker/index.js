0dc5a3d7f2a23554d36f16d371b4318354021143
Laura Schmidt <lauramarie.sdt@gmail.com>
2024-01-26 19:14:00 +0100

    ajout Math.random

--- password-maker/index.js
+++ password-maker/index.js
@@ -1,17 +1,18 @@
 const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
 const dataUppercase = dataLowercase.toUpperCase();
-const dataNumbers = "123456789";
+const dataNumbers = "0123456789";
 const dataSymbols = "$*&\"'()@!:;,.?=+";
+const rangeValue = document.getElementById("password-length");
 
 function generatePassword() {
     let data = [];
 
-    if (lowercase.checked) data.push(dataLowercase);
-    if (uppercase.checked) data.push(dataUppercase);
-    if (numbers.checked) data.push(dataNumbers);
-    if (symbols.checked) data.push(dataSymbols);
+    if (lowercase.checked) data.push(...dataLowercase);
+    if (uppercase.checked) data.push(...dataUppercase);
+    if (numbers.checked) data.push(...dataNumbers);
+    if (symbols.checked) data.push(...dataSymbols);
 
-    console.log(data);
+    console.log(data[Math.floor(Math.random() * data.length)]);
 }
 
 generateButton.addEventListener("click", generatePassword);
