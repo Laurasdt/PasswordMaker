10053fba4cac29cd5614af283fd21308c565f49d
Laura Schmidt <lauramarie.sdt@gmail.com>
2024-01-26 21:22:40 +0100

    it's working :)

--- password-maker/index.js
+++ password-maker/index.js
@@ -3,16 +3,40 @@ const dataUppercase = dataLowercase.toUpperCase();
 const dataNumbers = "0123456789";
 const dataSymbols = "$*&\"'()@!:;,.?=+";
 const rangeValue = document.getElementById("password-length");
+const passwordOutput = document.getElementById('password-output');
+
+console.log(passwordOutput);
 
 function generatePassword() {
     let data = [];
+    let password = "";
 
     if (lowercase.checked) data.push(...dataLowercase);
     if (uppercase.checked) data.push(...dataUppercase);
     if (numbers.checked) data.push(...dataNumbers);
     if (symbols.checked) data.push(...dataSymbols);
 
-    console.log(data[Math.floor(Math.random() * data.length)]);
+    if (data.length === 0) {
+        alert("Please select criteria ! ^^");
+        return;
+    }
+
+    for (i = 0; i < rangeValue.value; i++) {
+        password += data[Math.floor(Math.random() * data.length)];
+        console.log(password);
+    }
+
+    passwordOutput.value = password;
+
+    passwordOutput.select();
+    document.execCommand("copy");
+
+    generateButton.textContent = "Copied :p";
+
+    setTimeout(() => {
+        generateButton.textContent = "Generate a password"
+
+    }, 3000);
 }
 
 generateButton.addEventListener("click", generatePassword);
